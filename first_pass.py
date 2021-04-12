edabb96653f684827d8414be7fdb5a2a67d068f2
Nicholas <nraufiero@gmail.com>
2021-04-12 17:03:56 -0500

    Handling errors so that there is still output when a device does not list entities like macaddress, OS type.

--- first_pass.py
+++ first_pass.py
@@ -13,23 +13,49 @@ ip_addies = []
 nmap_out = nm.scan('192.168.0.*')
 for key, value in nmap_out['scan'].items():
     if key.startswith('192.168.0.'):
-        ip_dict['{ip}'.format(ip=key)] = value
-        ip_addies.append('{ip}'.format(ip=key))
+        print(key)
+        print(value)
+        ip_dict['ip'] = value
+        ip_addies.append(key)
 
-
+print(ip_addies)
 #NOTES: Let's change the overall table to key (mac address) : list (ip, product, ostype)
 
 device_dict = {}
 for ip in ip_addies:
     results = nmap.nmap_version_detection(ip)
+    print("results are /n")
+    print(results)
+    mac = results[ip]['macaddress']
+    print("mac address is /n")
+    print(mac)
+    stats = []
     try:
-        mac = results[ip]['macaddress']
-        stats = []
-        stats.append(results[ip]['ports'][1]['service']['name'])
-        stats.append(results[ip]['ports'][1]['service']['product'])
+        stats.append(results[ip]['ports'][0]['service']['name'])
+    except IndexError as e:
+        #if # what attributes does this error have and can we operate based on this knowledge?
+        #stats.append(results[ip]['ports'][1]['service']['name'])
+        pass
+    try:
+        stats.append(results[ip]['ports'][0]['service']['product'])
+    except:
+        #stats.append(results[ip]['ports'][1]['service']['product'])
+        pass
+    try:
         stats.append(results[ip]['ports'][1]['service']['ostype'])
+    except:
+        stats.append('os not described')
+    try:
         stats.append(results[ip]['ports'][1]['cpe'][0]['cpe'])
-        device_dict[mac] = stats
-        print(device_dict)
+    except:
+        #stats.append(results[ip]['ports'][2]['cpe'][0]['cpe'])
+        pass
+    try:
+        stats.append(results[ip]['macaddress']['vendor'])
     except:
         pass
+    try:
+        device_dict[mac['addr']] = stats
+    except:
+        device_dict['no mac, ip is {}'.format(ip)] = stats
+print(device_dict)
